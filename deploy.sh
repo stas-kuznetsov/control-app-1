1486bfe47a26253e63e70de6bbb0f19d3656975e
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-20 18:47:23 +0300

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 ansible-galaxy init app1-role
-mv ./hosts ./app1-role/test/
-mv ./test.yml ./app1-role/test/
+mv ./control-app-1/hosts ./app1-role/test/
+mv ./control-app-1/test.yml ./app1-role/test/
 ansible-playbook -i ./app1-role/test/hosts ./app1-role/test/test.yml
 
 
