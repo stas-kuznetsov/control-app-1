01433a88093e67f5e7573e71ad562156316b1f81
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-20 19:35:01 +0300

    modify test.yml

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 
 ansible-galaxy init app1-role
-mv ./control-app-1/hosts ./app1-role/tests/
-mv ./control-app-1/test.yml ./app1-role/tests/
-ansible-playbook -i ./app1-role/test/hosts ./app1-role/test/test.yml
+mv ./control-app-1/hosts ./app1-role/tests
+mv ./control-app-1/test.yml ./app1-role/tests
+ansible-playbook -i ./app1-role/tests/hosts ./app1-role/tests/test.yml
 
 
 
