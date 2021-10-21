810cdf26b01de2a60ea89174d9bc5c681a0afffc
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-21 20:46:14 +0300

    add main.yml

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,7 @@ username=jenkins
 
 ssh $username@$IPadr << EOF
 
-if [ "(docker ps -a -q)" != "" ]
-  then
-    docker stop $(docker ps -a -q)
-    docker rm $(docker ps -a -q)
-fi
-
-
 docker pull staskuznetsov/app1:$1
-docker run -d -p 8081:80 staskuznetsov/app1:$1
+docker run -d -p $1:80 staskuznetsov/app1:$1
+
 EOF
