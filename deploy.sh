b755e7c5cf3caff433d70eaaddb0370f961ae0ac
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-21 19:25:02 +0300

    add main.yml

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ IPadr=192.168.1.101
 username=jenkins
 
 ssh $username@$IPadr
-ssh $username@$IPadr ''
+
+ssh $username@$IPadr 'docker pull staskuznetsov/app1:$BUILD_NUMBER'
+ssh $username@$IPadr 'docker run -d -p 8081:80 staskuznetsov/app1:$BUILD_NUMBER'
 
