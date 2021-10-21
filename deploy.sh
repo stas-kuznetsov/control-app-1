e4d30f93eb1bb46e838368236faa6254c5712648
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-21 19:52:46 +0300

    add main.yml

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ IPadr=192.168.1.101
 username=jenkins
 
 ssh $username@$IPadr << EOF
+if [ "docker ps -a" -ne NULL ]
+  then
+    docker rm -f $(docker ps -a -q)
+fi
+
 
 docker pull staskuznetsov/app1:$1
 docker run -d -p 8081:80 staskuznetsov/app1:$1
