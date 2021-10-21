3e7249bfbede1bc5f02d0fa412110de8895c54bf
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-21 11:28:51 +0300

    add main.yml

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 ansible-galaxy init app1-role
 mv ./control-app-1/main.yml ./app1-role/tasks
-ansible-playbook -i control-app-1/hosts control-app-1/deploy.yml --ask-become-pass
+ansible-playbook -i control-app-1/hosts control-app-1/deploy.yml --extra-vars "ansible_sudo_pass=111111aA"
 
 
 
